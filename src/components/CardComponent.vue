e64f42c8460e149c2f9208d386065ba1c4cf89c8
SEKOU essolakina <essolakinasekou@gmail.com>
2023-01-24 17:18:03 +0000

    first commit

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -1,9 +1,7 @@
 <script setup>
 const prop = defineProps(['post','posts'])
-function deletePost(id){
-  let pos = prop.posts.find(p=>p.id == id);
-  console.log(pos.id);
-   prop.posts.splice(pos.id, 1)
+function deletePost(p){
+   prop.posts.splice(prop.posts.indexOf(p), 1)
   }
 </script>
 <template>
@@ -12,7 +10,7 @@ function deletePost(id){
               <h4 class="card-title">{{prop.post.title}}</h4>
               <p class="card-text"> {{prop.post.text }}</p>
               <a href="#" class="btn btn-primary">Voir plus</a>
-              <a :href="prop.post.id" @click.prevent="deletePost(prop.post.id)" class="btn btn-danger">supprimer</a>
+              <a :href="prop.post.id" @click.prevent="deletePost(prop.post)" class="btn btn-danger">supprimer</a>
             </div>
           </div>
 </template>
